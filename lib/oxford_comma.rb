d43ff98709a6df17330a488ac4385508e794bdac
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-14 22:10:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
     array[-1] = "and starfruit"
     array.join (", ")
   else array.length >= 4
-    array[-1] = "and dragon fruits"
+    array[-1] = "and #{array[-1]"
     array.join (", ")
     
   end
