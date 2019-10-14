b8e009f81fe5187bdabfe0cdb7d4103d1c534479
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-14 21:55:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,11 @@
 def oxford_comma(array)
   if array.length == 1 
     array[0]
- else array.length == 2
+ elsif array.length == 2
     array.join(" and ")
+    
+  elsif array.length == 3
   
-end
+  
+  end
 end
